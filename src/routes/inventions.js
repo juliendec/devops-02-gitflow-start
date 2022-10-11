394b6b14c28bb35cb65251af4ec7bfd9a3c0f5e7
juliendec <juliendecoen2308@gmail.com>
2022-10-11 17:22:02 +0200

    app progress

--- src/routes/inventions.js
+++ src/routes/inventions.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const Inventions = require('../models/Invention');
 const keyToUpperCase = require('../services/keyToUpperCase');
+const sortPerCreationDate = require('../services/sortPerCreationDate');
 
 const router = express.Router();
 
 router.get('/inventions/:key?', (req, res) => {
 	const inventions = keyToUpperCase(Inventions.list(), req.params.key ?? 'author');
+	res.send({ 
+		inventions, 
+		sources: [
+			'https://www.thoughtco.com/20th-century-timeline-1992486',
+			'https://en.wikipedia.org/wiki',
+		] 
+	});
+});
+
+router.get('/inventions/sort/:orderType?', (req, res) => {
+
+	const inventions = sortPerCreationDate(Inventions.list(), req.params.orderType);
 	res.send({ 
 		inventions, 
 		sources: [
---
+++ src/services/sortPerCreationDate.js
@@ -0,0 +1,5 @@
+module.exports = (items, sortType) => {
+    const sortedItems = items.sort((item1, item2) => item1.creationDate - item2.creationDate);
+
+    return sortType === 'asc' ? sortedItems : sortedItems.reverse();
+}
